fa54d731636ff5d6b83f6e63da7a1daa3a422969
arahatashun <arahatashun@gmail.com>
2018-05-05 22:11:09 +0900

    trua

--- demo.py
+++ demo.py
@@ -5,9 +5,9 @@
 Code for demonstration
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
-
-def genSynth(N, T, sig):
+def gen_synth(N, T, sig):
     """generate simulation data
 
     :param N: number of time series (a multiple of 4)
@@ -21,13 +21,20 @@ def genSynth(N, T, sig):
     assert N % 4 == 0, "N must be a multiple of 4"
     K = np.array([[0.9, 0, 0, 0], [1, 0.9, 0, 0], [1, 0, 0.9, 0], [1, 0, 0, 0.9]])
     A = np.kron(np.eye(int(N / 4)), K)
-    series = np.zeros((N, T))
+    series = np.zeros((N,T))
     series[:, 0] = np.random.randn(N)
-    for t in range(T-1):
+    for t in range(T - 1):
         series[:, t + 1] = A @ series[:, t] + sig * np.random.randn(N)
 
     return series, A
 
+def main():
+    N = 20 # number of time series
+    T = 100# length of time series
+    sig = 0.2
+    series, A = gen_synth(N, T, sig)
+    plt.spy(A)
+    plt.show()
 
 if __name__ == '__main__':
-    genSynth(4, 10, 0.1)
+    main()
