79631abd765b31367e3f73f0c65a4678422044de
arahatashun <arahatashun@gmail.com>
2018-05-06 19:50:47 +0900

    tura

--- lassoGranger.py
+++ lassoGranger.py
@@ -37,7 +37,8 @@ def lasso_granger(series, P, alpha):
     n1Coeff = np.zeros((N, P))
     for i in range(N):
         n1Coeff[i, :] = vals2[i * P:(i + 1) * P]
-        sumCause = np.sum(np.abs(n1Coeff), axis=1)
-        sumCause[sumCause < th] = 0
-        cause = sumCause
-        return cause
+    
+    sumCause = np.sum(np.abs(n1Coeff), axis=1)
+    sumCause[sumCause < th] = 0
+    cause = sumCause
+    return cause
