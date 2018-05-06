66772613e2c68507b793c3f432376848fc552a21
arahatashun <arahatashun@gmail.com>
2018-05-06 19:47:49 +0900

    tura

--- lassoGranger.py
+++ lassoGranger.py
@@ -21,23 +21,23 @@ def lasso_granger(series, P, alpha):
     N, T = np.shape(series)
     Am = np.zeros((T - P, P * N))
     bm = np.zeros((T - P, 1))
-    for i in np.arange(P + 1, T):
-        bm[i - P] = series[1, i]
+    for i in range(P, T - 1):
+        bm[i - P] = series[0, i + 1]
         Am[i - P, :] = np.fliplr(series[:, i - P:i]).flatten()
 
     # Lasso using GLMnet
-    fit = glmnet(x=Am, y=bm, family='gaussian', alpha=1, lambda_min=np.array([alpha]))
+    fit = glmnet(x=Am, y=bm, family='gaussian', alpha=1, lambdau=np.array([alpha]))
     vals2 = fit['beta']  # array of coefficient
 
     # Outputting aic metric for variable into (N,P) matrix
     th = 0
-    aic = (LA.norm(Am @ vals2 - bm, 2))**2 / (T - P) + np.sum(np.abs(vals2) > th) * 2 / (T - P)
+    aic = (LA.norm(Am @ vals2 - bm, 2)) ** 2 / (T - P) + np.sum(np.abs(vals2) > th) * 2 / (T - P)
 
     # Reformatting the results into (N,P) matrix
     n1Coeff = np.zeros((N, P))
     for i in range(N):
-        n1Coeff[i, :] = vals2[(i - 1) * P, i * P - 1]
-    sumCause = np.sum(np.abs(n1Coeff), axis=1)
-    sumCause[sumCause < th] = 0
-    cause = sumCause
-    return cause
+        n1Coeff[i, :] = vals2[i * P:(i + 1) * P]
+        sumCause = np.sum(np.abs(n1Coeff), axis=1)
+        sumCause[sumCause < th] = 0
+        cause = sumCause
+        return cause
