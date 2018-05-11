2358cdc696bdb42ba37390b2ee29eadb92a5e7e0
Shun Arahata <arahatashun@gmail.com>
2018-05-12 01:00:24 +0900

    turai

--- demo.py
+++ demo.py
@@ -161,7 +161,7 @@ def main():
 
 def test1():
     N = 20
-    T = 100
+    T = 1000
     sig = 0.2
     series, A = gen_synth_lagged(N, T, sig)
     # Run Lasso-Granger
@@ -192,18 +192,18 @@ def test1():
 
 def test2():
     N = 20
-    T = 1000
+    T = 100
     sig = 0.1
     series, A_array = gen_synth_lagged(N, T, sig)
     # Run Lasso-Granger
-    alpha = 1e-1
+    alpha = 1e-2
     cause = np.zeros((N, N, 3))
     series = inject_nan(series, 0.1)
+    cell_array = gen_list_iLasso(series,np.arange(series.shape[1]))
     for i in range(N):
-        index = [i] + list(range(i)) + list(range(i + 1, N))
-        cell_array = gen_list_iLasso(series[index, :],
-                                     np.arange(series.shape[1]))
-        cause_tmp = ilasso(cell_array, alpha)
+        order = [i] + list(range(i)) + list(range(i + 1, N))
+        new_cell = [ cell_array[i] for i in order]
+        cause_tmp = ilasso(new_cell, alpha)
         index = list(range(1, i + 1)) + [0] + list(range(i + 1, N))
         cause[i, :, :] = cause_tmp[index, :]
 
@@ -213,7 +213,7 @@ def test2():
     ax1.spy(A_array[0])
     ax1.set_title('Ground Truth')
     ax1.matshow(A_array[0], cmap=plt.cm.Blues)
-    ax2.matshow(cause[:, :, 0], cmap=plt.cm.Blues)
+    ax2.matshow(cause[:, :, 2], cmap=plt.cm.Blues)
     ax2.set_title('Inferred Causality')
     plt.show()
     fig, axs = plt.subplots(1, 2)
@@ -228,7 +228,7 @@ def test2():
     ax1 = axs[0]
     ax2 = axs[1]
     ax1.matshow(A_array[2], cmap=plt.cm.Blues)
-    ax2.matshow(cause[:, :, 2], cmap=plt.cm.Blues)
+    ax2.matshow(cause[:, :, 0], cmap=plt.cm.Blues)
     ax1.set_title('Ground Truth')
     ax2.set_title('Inferred Causality')
     plt.show()
