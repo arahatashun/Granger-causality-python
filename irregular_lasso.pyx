b172da0c4f0f4b20e6e8ed35ed24a57bb99e2cfd
Shun Arahata <arahatashun@gmail.com>
2018-05-13 01:24:00 +0900

    tura

--- irregular_lasso.pyx
+++ irregular_lasso.pyx
@@ -11,6 +11,7 @@ Example:
 import numpy as np
 cimport numpy as np
 cimport cython
+
 np.import_array()
 import glmnet_python
 from glmnet import glmnet
@@ -18,7 +19,6 @@ from numpy import linalg as LA
 
 DTYPE = np.double
 ctypedef np.double_t DTYPE_t
-
 
 def irregular_lasso(cell_list, alpha):
     """
@@ -40,18 +40,18 @@ def irregular_lasso(cell_list, alpha):
     cdef double SIG = 0.1  # Kernel parameter. Here Gaussian Kernel Bandwidth
     # index of last time which is less than L*Dt - 1
     cdef int B = np.argmax(cell_list[0][1, :] > L * Dt)
-    assert B > 0, " L DT error"
+    # assert B > 0, " L DT error"
     # number of index of time of explained variable
     cdef int N1 = cell_list[0][1].shape[0]
     # number of features
     cdef int P = len(cell_list)
 
     # Build the matrix elements
-    cdef np.ndarray[DTYPE_t, ndim=2] Am = np.zeros(
-        (N1 - B, P * L))  # explanatory variables
-    cdef np.ndarray[DTYPE_t, ndim=2] bm = cell_list[0][0, B:N1 + 1].reshape(
-        (N1 - B, 1))
+    cdef np.ndarray[DTYPE_t, ndim=2] Am = np.zeros((N1 - B, P * L))  # explanatory variables
+    cdef np.ndarray[DTYPE_t, ndim=2] bm = cell_list[0][0, B:N1 + 1].reshape((N1 - B, 1))
     # for loop for stored time stamp
+    cdef i
+    cdef j
     for i in range(B, N1):
         ti = np.arange((cell_list[0][1, i] - L * Dt),
                        (cell_list[0][1, i] - Dt) + Dt, Dt)
@@ -59,13 +59,10 @@ def irregular_lasso(cell_list, alpha):
         for j in range(P):
             assert len(ti) == L, "length does not match"
             tij = np.broadcast_to(ti, (len(cell_list[j][1, :]), ti.size))
-            tSelect = np.broadcast_to(cell_list[j][1, :],
-                                      (L, cell_list[j][1, :].size)).T
-            ySelect = np.broadcast_to(cell_list[j][0, :],
-                                      (L, cell_list[j][0, :].size)).T
+            tSelect = np.broadcast_to(cell_list[j][1, :], (L, cell_list[j][1, :].size)).T
+            ySelect = np.broadcast_to(cell_list[j][0, :],(L, cell_list[j][0, :].size)).T
             # kernel is used as window function??
-            Kernel = np.exp(
-                -(np.multiply((tij - tSelect), (tij - tSelect)) / SIG))
+            Kernel = np.exp( -(np.multiply((tij - tSelect), (tij - tSelect)) / SIG))
             Am[i - B, (j * L):(j + 1) * L] = np.divide(
                 np.sum(np.multiply(ySelect, Kernel), axis=0),
                 np.sum(Kernel, axis=0))
