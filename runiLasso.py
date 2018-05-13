3b3b9a4b9113c78db2b52348d3f1a2a5f81338a8
Shun Arahata <arahatashun@gmail.com>
2018-05-13 18:54:13 +0900

    tura

--- runiLasso.py
+++ runiLasso.py
@@ -10,30 +10,31 @@ from multiprocessing import Pool
 
 
 def solve_loop(cell_array, alpha):
-    """solve irrgegular lasso in parallel
+    """solve irregular lasso in parallel
 
     :param cell_array:one cell for each time series. Each cell is a 2xT matrix.
-    First row contains the values and the second row contains SORTED time stamps.
-    The first time series is the target time series which is predicted.
+        First row contains the values and the second row contains SORTED time stamps.
+        The first time series is the target time series which is predicted.
+    :param alpha: The regularization parameter in Lasso
     """
-    N = len(cell_array)
-    cause = np.zeros((N, N, 3))
-    argu_for_process = []
-    for i in range(N):
-        order = [i] + list(range(i)) + list(range(i + 1, N))
+    total_features = len(cell_array)
+    cause = np.zeros((total_features, total_features, 3))
+    argument_for_process = []
+    for i in range(total_features):
+        order = [i] + list(range(i)) + list(range(i + 1, total_features))
         new_cell = [cell_array[i] for i in order]
-        argu_for_process.append((new_cell, i, N, alpha))
+        argument_for_process.append((new_cell, i, total_features, alpha))
     pool = Pool()
-    output = pool.map(wrap_worker, argu_for_process)
-    for i in range(N):
+    output = pool.map(wrap_worker, argument_for_process)
+    for i in range(total_features):
         cause[i, :, :] = output[i][0]
     return cause
 
 
-def process_worker(new_cell, i, N, alpha):
+def process_worker(new_cell, i, n, alpha):
     cause_tmp, aic, bic = irregular_lasso(new_cell, alpha)
-    index = list(range(1, i + 1)) + [0] + list(range(i + 1, N))
-    return cause_tmp[index, :],aic,bic
+    index = list(range(1, i + 1)) + [0] + list(range(i + 1, n))
+    return cause_tmp[index, :], aic, bic
 
 
 def wrap_worker(arg):
