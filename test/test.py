2c04527702ae706c9be6682ecc5afa9828703b8f
arahatashun <arahatashun@gmail.com>
2018-05-16 10:19:58 +0900

    test

---
+++ test/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+# Author: Shun Arahata
+
+import sys
+sys.path.append('../granger_python')
+import numpy as np
+from run_ilasso import solve_loop
+
+cell = np.load('data.npy')
+alpha = 1e-2
+cause = solve_loop(cell, alpha, 3)
+
+np.savetxt('result3.csv', cause[:, :, 0], delimiter=',')
+np.savetxt('result2.csv', cause[:, :, 1], delimiter=',')
+np.savetxt('result1.csv', cause[:, :, 2], delimiter=',')
