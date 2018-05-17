a3dcdfcd9c866c05214f690081377a252d5df7cf
arahatashun <arahatashun@gmail.com>
2018-05-18 00:07:55 +0900

    tura

--- granger_python/run_ilasso.py
+++ granger_python/run_ilasso.py
@@ -10,16 +10,14 @@ from multiprocessing import Pool
 from tqdm import tqdm
 from ilasso import ilasso
 
-def solve_loop(cell_array, alpha, sigma, lag_len, dt):
+def solve_loop(cell_array, alpha, lag_len):
     """solve irregular lasso in parallel
 
     :param cell_array:one cell for each time series. Each cell is a 2xT matrix.
         First row contains the values and the second row contains SORTED time stamps.
         The first time series is the target time series which is predicted.
     :param alpha: The regularization parameter in Lasso
-    :param sigma:kernel parameter. Here Gaussian kernel Bandwidth
     :param lag_len:Length of studied lag
-    :param dt:the  average  length  of  the  sampling  intervals for the target time series
     """
     total_features = len(cell_array)
     cause = np.zeros((total_features, total_features, lag_len))
@@ -27,6 +25,7 @@ def solve_loop(cell_array, alpha, sigma, lag_len, dt):
     for i in range(total_features):
         num_of_element = len(cell_array[i][0])
         avg_dt = (cell_array[i][1][-1] - cell_array[i][1][0])/num_of_element
+        sigma = avg_dt/4 # Comparison of correlation analysis techniques for irregularly sampled time series
         order = [i] + list(range(i)) + list(range(i + 1, total_features))
         new_cell = [cell_array[i] for i in order]
         argument_for_process.append(
