304f372812c8f97411057f295f38753ab37a7c51
Shun Arahata <arahatashun@gmail.com>
2018-06-13 20:40:42 +0900

    tura

--- granger_python/graph_compare.py
+++ granger_python/graph_compare.py
@@ -13,8 +13,8 @@ import matplotlib.axes
 def f_score(a, b, threshold=0.1):
     """ Precision Recall F1 Score
 
-    :param a: Matirx
-    :param b: Matirx
+    :param a: train
+    :param b: test
     :return:
     """
     a = np.copy(a)
@@ -28,14 +28,14 @@ def f_score(a, b, threshold=0.1):
     b_tmp[b < threshold] = -1
     true_positive_mat[b_tmp == a] = 1
     true_positive = np.sum(true_positive_mat)
-    precison = true_positive / np.sum(a)
-    recall = true_positive / np.sum(b)
+    precison = true_positive / np.sum(b)
+    recall = true_positive / np.sum(a)
     score = 2 * precison * recall / (precison + recall)
     diff_mat = np.zeros_like(a)
     diff_mat[a >= threshold] += 1
     diff_mat[b >= threshold] += 2
 
-    return score, diff_mat
+    return precision, recall, score, diff_mat
 
 
 def generate_cmap(colors):
