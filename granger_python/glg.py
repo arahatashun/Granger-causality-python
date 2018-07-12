cbbee7ea7113cbb98b8f0059af056ab445be1e38
Shun Arahata <arahatashun@gmail.com>
2018-07-12 13:25:45 +0900

    tura

---
+++ granger_python/glg.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+# Author: Shun Arahata
+"""
+Code for Generalized lasso Granger
+"""
+import numpy as np
+from glmnet import glmnet
+from numpy import linalg as LA
+
+
+class GLG:
+    def __init__(self, cell_list, sigma, lag_len, dt):
+        """ starts pre processing
+        :param cell_list:one cell for each time series. Each cell is a 2xT matrix.
+        First row contains the values and the second row contains SORTED time stamps.
+        The first time series is the target time series which is predicted.
+        :param alpha:The regularization parameter in Lasso
+        :param sigma:Kernel parameter. Here Gaussian Kernel Bandwidth
+        :param lag_len: Length of studied lag
+        :param dt:Delta t denotes the  average  length  of  the  sampling  intervals for the target time series
+        """
+        # index of last time which is less than lag_len*dt　- 1
+        B = np.argmax(cell_list[0][1, :] > lag_len * dt)
+        assert B >= 0, " lag_len DT error"
+        # number of index of time of explained variable
+        N1 = cell_list[0][1].shape[0]
+        # number of features
+        P = len(cell_list)
+        # Build the matrix elements
+        Am = np.zeros((N1 - B, P * lag_len))  # explanatory variables
+        bm = cell_list[0][0, B:N1 + 1].reshape((N1 - B, 1))
+        # for loop for stored time stamp
+        for i in range(B, N1):
+            ti = np.linspace((cell_list[0][1, i] - lag_len * dt), cell_list[0][1, i] - dt, num=lag_len)
+            # for loop for features
+            for j in range(P):
+                assert len(ti) == lag_len, str(len(ti)) + str(lag_len) + "length does not match"
+                """
+                tij = np.broadcast_to(ti, (len(cell_list[j][1, :]), ti.size))
+                tSelect = np.broadcast_to(cell_list[j][1, :],
+                                          (lag_len, cell_list[j][1, :].size)).T
+                ySelect = np.broadcast_to(cell_list[j][0, :],
+                                          (lag_len, cell_list[j][0, :].size)).T
+                """
+                # reduce kernel length in order to reduce complexity of calculation
+                # kernel is used as window function
+                # time_match is a cell_list[time_match][1, :] nearest to tij
+                time_match = np.searchsorted(cell_list[j][1, :], cell_list[0][1, i])
+                kernel_length = 25  # half of kernel length
+                start = time_match - kernel_length if time_match - kernel_length > 0 else 0
+                end = time_match + kernel_length if time_match + kernel_length < len(cell_list[j][1, :]) - 1 else len(
+                    cell_list[j][1, :])
+                tij = np.broadcast_to(ti, (len(cell_list[j][1, start:end]), ti.size))
+                tSelect = np.broadcast_to(cell_list[j][1, start:end],
+                                          (lag_len, cell_list[j][1, start:end].size)).T
+                ySelect = np.broadcast_to(cell_list[j][0, start:end],
+                                          (lag_len, cell_list[j][0, start:end].size)).T
+                exponent = -(np.multiply((tij - tSelect), (tij - tSelect)) / 2 * sigma ** 2)
+                # assert np.isfinite(exponent).all() == 1, str(exponent)
+                Kernel = np.exp(exponent)
+                # assert np.isfinite(Kernel).all() == 1, str(Kernel)
+                with np.errstate(divide='ignore'):
+                    ker_sum = np.sum(Kernel, axis=0)
+                    numerator = np.sum(np.multiply(ySelect, Kernel), axis=0)
+                    # assert np.isfinite(numerator).all() ==1,str(numerator)
+                    # assert np.isfinite(ker_sum).all() ==1,str(ker_sum)
+                    tmp = np.divide(numerator, ker_sum)
+                    tmp[ker_sum == 0] = 1
+                    # assert (np.isfinite(tmp)).all() == 1,str(tmp)+str(ker_sum)
+                """
+                if np.sum(Kernel, axis=0).any() == 0:
+                    print("kernel zero" + str(np.sum(Kernel, axis=0)))
+                    print(tmp)
+                """
+                Am[i - B, (j * lag_len):(j + 1) * lag_len] = tmp
+            self.Am = Am
+            self.bm = bm
+            self.lag_len = lag_len
+            self.P = P
+            self.N1 = N1
+            self.B = B
+
+    def calculate(self,alpha):
+        Am = self.Am
+        bm = self.bm
+        fit = glmnet(x=Am, y=bm, family='gaussian', alpha=1, lambdau=np.array([alpha]))
+        weight = fit['beta']  # array of coefficient
+        # Reformatting the output
+        result = np.zeros((self.P, self.lag_len))
+        for i in range(self.P):
+            result[i, :] = weight[i * self.lag_len:(i + 1) * self.lag_len].ravel()
+
+        return result
+
+    def crossvalidate(self,alpha):
+        Am = self.Am
+        bm = self.bm
+        last_index = int((self.N1 - self.B) * 0.7)
+        Am_train = Am[:last_index]
+        bm_train = bm[:last_index]
+        Am_test = Am[last_index:]
+        bm_test = bm[last_index:]
+        fit = glmnet(x=Am_train, y=bm_train, family='gaussian', alpha=1, lambdau=np.array([alpha]))
+        weight = fit['beta']  # array of coefficient
+        error = LA.norm(Am_test @ weight - bm_test) ** 2 / (self.N1 - self.B - last_index)
+
+        # Reformatting the output
+        result = np.zeros((self.P, self.lag_len))
+        for i in range(self.P):
+            result[i, :] = weight[i * self.lag_len:(i + 1) * self.lag_len].ravel()
+
+        return result, error
--- granger_python/run_granger.py
+++ granger_python/run_granger.py
@@ -13,15 +13,14 @@ from ilasso import ilasso
 from glg import GLG
 
 
-def solve_parallel(cell_array, alpha, lag_len, cv=False, group=False):
+def solve_parallel(cell_array, alpha, lag_len, group=False):
     """solve Granger lasso in parallel
 
     :param cell_array:one cell for each time series. Each cell is a 2xT matrix.
         First row contains the values and the second row contains SORTED time stamps.
         The first time series is the target time series which is predicted.
-    :param alpha: The regularization parameter in Lasso single value 
+    :param alpha: The regularization parameter in Lasso single value
     :param lag_len:Length of studied lag
-    :param cv:whether or not do cross validation
     :param group:group lasso
     """
     total_features = len(cell_array)
@@ -35,7 +34,7 @@ def solve_parallel(cell_array, alpha, lag_len, cv=False, group=False):
         sigma = avg_dt / 4  # Comparison of correlation analysis techniques for irregularly sampled time series
         order = [i] + list(range(i)) + list(range(i + 1, total_features))
         new_cell = [cell_array[i] for i in order]
-        argument_for_process.append((new_cell, i, total_features, alpha, sigma, lag_len, avg_dt, cv, group))
+        argument_for_process.append((new_cell, i, total_features, alpha, sigma, lag_len, avg_dt, group))
     # start multiprocessing
     pool = Pool()
     outputs = []
@@ -44,47 +43,26 @@ def solve_parallel(cell_array, alpha, lag_len, cv=False, group=False):
         bar.update()
         outputs.append(output)
     print(bar)
-    if cv is False:
-        for i in range(total_features):
-            j = outputs[i]['index']
-            cause[j, :, :] = outputs[i]['cause']
-        print("alpha:", alpha, ", lag:", lag_len)
-        return cause
-    else:
-        error = 0
-        for i in range(total_features):
-            j = outputs[i]['index']
-            cause[j, :, :] = outputs[i]['cause']
-            error += outputs[i]['error']
-        print("alpha:", alpha, ", lag:", lag_len,",Error:", error)
-        return cause, error
+    for i in range(total_features):
+        j = outputs[i]['index']
+        cause[j, :, :] = outputs[i]['cause']
+    print(cause.shape)
+    return cause
 
 
-def process_worker(new_cell, i, n, alpha, sigma, lag_len, dt, cv, group):
-    if cv is False:
-        if group is False:
-            glg = GLG(new_cell, sigma, lag_len, dt)
-            cause_tmp = GLG.calculate(alpha)
-            index = list(range(1, i + 1)) + [0] + list(range(i + 1, n))
-            dict = {'cause': cause_tmp[index, :], 'index': i}
-            return dict
-        if group is True:
-            cause_tmp = igrouplasso(new_cell, alpha, sigma, lag_len, dt, cv)
-            index = list(range(1, i + 1)) + [0] + list(range(i + 1, n))
-            dict = {'cause': cause_tmp[index, :], 'index': i}
-            return dict
-    else:
-        if group is False:
-            glg = GLG(new_cell, sigma, lag_len, dt)
-            cause_tmp, error = glg.crossvalidate(alpha)
-            index = list(range(1, i + 1)) + [0] + list(range(i + 1, n))
-            dict = {'cause': cause_tmp[index, :], 'index': i, 'error': error}
-            return dict
-        if group is True:
-            cause_tmp, error = igrouplasso(new_cell, alpha, sigma, lag_len, dt, cv)
-            index = list(range(1, i + 1)) + [0] + list(range(i + 1, n))
-            dict = {'cause': cause_tmp[index, :], 'index': i, 'error': error}
-            return dict
+def process_worker(new_cell, i, n, alpha, sigma, lag_len, dt, group):
+    if group is False:
+        glg = GLG(new_cell, sigma, lag_len, dt)
+        cause_tmp = glg.calculate(alpha)
+        index = list(range(1, i + 1)) + [0] + list(range(i + 1, n))
+        dict = {'cause': cause_tmp[index, :], 'index': i}
+        return dict
+    if group is True:
+        cause_tmp = igrouplasso(new_cell, alpha, sigma, lag_len, dt)
+        index = list(range(1, i + 1)) + [0] + list(range(i + 1, n))
+        dict = {'cause': cause_tmp[index, :], 'index': i}
+        return dict
+
 
 
 def wrap_worker(arg):
@@ -93,7 +71,7 @@ def wrap_worker(arg):
 
 
 def search_optimum_lambda(cell_array, lambda_min, lambda_max, lag_len, group=False, grid=20):
-    """ search optimum lambda
+    """ search optimum lambda parallel
 
     :param cell_array:
     :param lambda_max:
@@ -101,19 +79,44 @@ def search_optimum_lambda(cell_array, lambda_min, lambda_max, lag_len, group=Fal
     :param group:
     :return:
     """
-    cv_error = []
-    lambda_list = []
+    cv_error = np.zeros(grid)
     lambda_exponent = np.linspace(np.log10(lambda_min), np.log10(lambda_max), grid)
-    for i, j in enumerate(lambda_exponent):
-        print(i)
-        _, error = solve_parallel(cell_array, 10 ** j, lag_len, cv=True, group=group)
-        lambda_list.append(j)
-        cv_error.append(error)
+    total_features = len(cell_array)
+    #for loop for featuers
+    bar = pyprind.ProgBar(total_features*grid, width=60, bar_char='#', title='PROGRESS')
+    for i in range(total_features):
+        num_of_element = len(cell_array[i][0])
+        avg_dt = (cell_array[i][1][-1] - cell_array[i][1][0]) / num_of_element
+        assert avg_dt > 0, "avg_dt:" + str(avg_dt) + ", num_elem:" + str(num_of_element) + ", index:" + str(i)
+        sigma = avg_dt / 4  # Comparison of correlation analysis techniques for irregularly sampled time series
+        order = [i] + list(range(i)) + list(range(i + 1, total_features))
+        new_cell = [cell_array[i] for i in order]
+        if group is False:
+            glg: object = GLG(new_cell, sigma, lag_len, avg_dt)
+        argument_for_process = [(glg, 10 ** lambda_exponent[i], i) for i in range(grid)]
+        # Parallel calculation for cross validation
+        pool = Pool()
+        for _, output in enumerate(pool.imap_unordered(cv_wrap_worker, argument_for_process)):
+            bar.update()
+            cv_error[output['index']] += output['error']
 
-    optimum = lambda_list[np.argmin(cv_error)]
-
+    print(bar)
+    optimum = lambda_exponent[np.argmin(cv_error)]
     optimum = 10 ** optimum
     print("Optimum lambda", optimum)
-    plt.scatter(lambda_list, cv_error)
-    plt.savefig("lambda_search.png")
+    plt.scatter(lambda_exponent, cv_error)
+    plt.show()
+    #plt.savefig("lambda_search.png")
     return optimum
+
+
+def cv_process_worker(glg, alpha, i):
+    result, error = glg.crossvalidate(alpha)
+    dict = {'cause': result, 'error': error, 'index': i}
+    return dict
+
+
+def cv_wrap_worker(arg):
+    """wrapper function"""
+    return cv_process_worker(*arg)
+
